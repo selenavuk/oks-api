074f8be61d6698a387ec00c8403a5c24f77d82b3
Selena Vukasinovic <64490758+selenavuk@users.noreply.github.com>
2024-09-15 15:45:43 +0200

    google auth edited

--- src/main/java/rs/oks/api/CorsConfig.java
+++ src/main/java/rs/oks/api/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://www.rhv.rs")
+                        .allowedOrigins("http://localhost:4200", "https://www.rhv.rs", "https://oks-api-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("Content-Type", "Authorization")
                         .allowCredentials(true);
