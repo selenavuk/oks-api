132f04efb429a8f4a00e34f7825a8f8b80b23e70
Selena Vukasinovic <64490758+selenavuk@users.noreply.github.com>
2024-09-27 22:06:04 +0200

    google auth edited (debug added)

--- src/main/java/rs/oks/api/CorsConfig.java
+++ src/main/java/rs/oks/api/CorsConfig.java
@@ -11,20 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://www.rhv.rs", "https://oks-api-production.up.railway.app", "https://accounts.google.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("Content-Type", "Authorization")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("https://localhost:4200", "https://www.rhv.rs", "https://oks-api-production.up.railway.app", "https://accounts.google.com")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////                        .allowedHeaders("Content-Type", "Authorization")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
 
     @Bean
     public CorsFilter corsFilter() {
