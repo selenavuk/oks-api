65ce2354622b2310bb728e4af1290840520e8501
Selena Vukasinovic <64490758+selenavuk@users.noreply.github.com>
2024-10-17 01:22:54 +0200

    try new credential - oks-rest-api-2

--- src/main/java/rs/oks/api/misc/classes/GoogleClientSecretsConfig.java
+++ src/main/java/rs/oks/api/misc/classes/GoogleClientSecretsConfig.java
@@ -22,12 +22,14 @@ public class GoogleClientSecretsConfig {
 
     private static final String CLIENT_SECRETS_JSON = "{\n" +
             "  \"web\": {\n" +
-            "    \"client_id\": \"72055840890-brmi6c7jn2fnj8830cqek5gjp4aomlj6.apps.googleusercontent.com\",\n" +
+//            "    \"client_id\": \"72055840890-brmi6c7jn2fnj8830cqek5gjp4aomlj6.apps.googleusercontent.com\",\n" +
+            "    \"client_id\": \"72055840890-1lq48c0k28crj2iht92b7s6rjb110kb6.apps.googleusercontent.com\",\n" +
             "    \"project_id\": \"oks-rest-api-435116\",\n" +
             "    \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n" +
             "    \"token_uri\": \"https://oauth2.googleapis.com/token\",\n" +
             "    \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n" +
-            "    \"client_secret\": \"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\",\n" +
+//            "    \"client_secret\": \"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\",\n" +
+            "    \"client_secret\": \"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\",\n" +
             "    \"redirect_uris\": [\"http://localhost:59401/Callback\", \"https://oks-api-production.up.railway.app/Callback\"]\n" +
             "  }\n" +
             "}";
